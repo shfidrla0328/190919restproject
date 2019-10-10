112f96032afcb89a1ed1e9b4063aad863049c43b
shfidrla0328 <shfidrla@gmail.com>
2019-10-10 20:52:31 +0900

    restframeworkAPI

--- rest/views.py
+++ rest/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import viewsets
+import requests
+import json
+
 
 # Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    url = "https://kapi.kakao.com/v1/translation/translate" 
+    queryString = {"query":"당신은 나를 사랑합니까?", "src_lang":"kr", "target_lang":"en"}
+    header = {"Authorization": "KakaoAK 1155da90c9368c49bc216b5aa562f432"}
+    r = requests.get(url, headers=header, params=queryString)
+    print(json.loads(r.text))
